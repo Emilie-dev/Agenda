a7092d7ff246e8d8ad57d0943b5feb223f109dd6
Chauvin <chauvin-e@orange.fr>
2017-03-30 10:52:05 +0200

    ajout d'objet

---
+++ app.js
@@ -0,0 +1,22 @@
+var Axel = {
+	"nom": "Dupont",
+	"prénom": "Axel",
+	"âge": 34
+};
+console.log(Axel.nom);
+
+var Sophie = {
+	"nom": "Durand",
+	"prénom": "Sophie",
+	"âge": 21
+};
+console.log(Sophie[1]);
+
+var Arthur = {
+	"nom": "Legrand",
+	"prénom": "Arthur",
+	"âge": 46
+};
+console.log(Arthur["âge"]);
+
+var agenda = {"Axel":Axel, "Sophie":Sophie, "Arthur":Arthur};
