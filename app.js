3b69264f01cf83360d5396f7c4993e5f31a89c99
Chauvin <chauvin-e@orange.fr>
2017-03-31 17:11:58 +0200

    Je continue l'exercice.

--- app.js
+++ app.js
@@ -23,38 +23,39 @@ var agenda = {"Axel":Axel, "Sophie":Sophie, "Arthur":Arthur};
 console.log(Axel.prénom);
 
 $("button").click(function() {
+
 	$("input").change(function() {
+
 	$("input").append($("agenda").val());
+	
+	});
+
 });
 
 var annuaire = [];
 
 $("#valider").click(function() {
 	var contact1 = $("#firstname").val();
-	console.log(contact1);
+	// console.log(contact1);
 	var contact2 = $("#surname").val();
-	console.log(contact2);
+	// console.log(contact2);
 	var contact3= $("#old").val();
-	console.log(contact3);
+	// console.log(contact3);
 
 var person = {"nom": contact1, "prenom": contact2, "age":contact3};
 
 annuaire.push(person);
-console.log(annuaire);
 
 $('input').val(' ');   // efface le texte inscrit dans les inputs automatiquement
 
 $("#liste").append('<tr><td>'+person.nom+'</td><td>'+person.prenom+'</td><td>'+person.age+'</td><td><button class="supr">Delete</button>'); // ajout d'un bouton "Delete".
 																																			// <button class="supr">Delete</button>.
-
-
-
-
 });
+
 
 $('#liste').delegate(".supr", "click", function() {  
-	$(this).parent().parent().remove();      /* On supprime le parent du parent à partir du bouton "Delete",
-												on part du bouton ".supr"("Delete"), puis 1° parent().=<td> et 2° parent().=<tr>.*/
-});
+	$(this).parent().parent().remove();      // On supprime le parent du parent à partir du bouton "Delete".On part du bouton ".supr"("Delete"), puis 1° parent().=<td> et 2° parent().=<tr>.
 
+										
+});
 
